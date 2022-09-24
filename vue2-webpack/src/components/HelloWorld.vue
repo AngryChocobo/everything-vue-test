01aa148d2d516bc44eb4e3291891a303ced3f63f
jupengfei <1347237169@qq.com>
2022-09-24 18:17:25 +0800

    add element for vue2

--- vue2-webpack/src/components/HelloWorld.vue
+++ vue2-webpack/src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <el-button type="primary">{{ msg }}</el-button>
   </div>
 </template>
 
--- vue2-webpack/tests/unit/example.spec.ts
+++ vue2-webpack/tests/unit/example.spec.ts
@@ -1,11 +1,15 @@
-import { shallowMount } from "@vue/test-utils";
+import { shallowMount, createLocalVue } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import Element from "element-ui";
+const localVue = createLocalVue();
+localVue.use(Element);
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
     const msg = "new message";
     const wrapper = shallowMount(HelloWorld, {
       propsData: { msg },
+      localVue,
     });
     expect(wrapper.text()).toMatch(msg);
   });
