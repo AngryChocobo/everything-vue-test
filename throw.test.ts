747b1b19fd8a47ec3e9416862c6ec2867f98e684
jupengfei <1347237169@qq.com>
2022-12-14 15:36:17 +0800

    refactor: remove root dir useless files

--- throw.test.ts
+++
@@ -1,8 +0,0 @@
-test("should throw a error", () => {
-  function throw1() {
-    throw new Error("123");
-  }
-  expect(() => throw1()).toThrow();
-  expect(() => throw1()).toThrow("123");
-  expect(() => throw1()).toThrow(Error);
-});
