fbadb07e99a25435a13896a8d59dccf182bb13b0
jupengfei <1347237169@qq.com>
2022-12-20 17:35:07 +0800

    chore: change tsconfig

--- shared/.eslintrc.js
+++ shared/.eslintrc.js
@@ -1,17 +1,1 @@
-module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true,
-        "node": true
-    },
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-};
+module.exports = {};
