7c27bdb8948b11d0010b8ca0da8ef6ddc190b649
AngryChocobo <1347237169@qq.com>
2022-12-24 11:50:45 +0800

    feat: pinia test  for vue3

--- vue3/src/components/todos/__tests__/TodoAppStore.cy.ts
+++ vue3/src/components/todos/__tests__/TodoAppStore.cy.ts
@@ -1,26 +1,64 @@
-import { createTestingPinia } from "@pinia/testing";
 import { useTodoList } from "@/stores/todos";
-import { createVNode, defineComponent } from "vue";
+import { setActivePinia, createPinia } from "pinia";
+import { TodoFilterEnum } from "shared";
 
 describe("store", () => {
-  const piniaSpy = cy.spy().as("piniaSpy");
+  beforeEach(() => {
+    // creates a fresh pinia and make it active so it's automatically picked
+    // up by any useStore() call without having to pass it to it:
+    // `useStore(pinia)`
+    setActivePinia(createPinia());
+  });
+  it("should have default state", () => {
+    const store = useTodoList();
+    expect(store.list.length).to.equal(0);
+  });
+  it("should add todo ", () => {
+    const store = useTodoList();
+    expect(store.list.length).to.equal(0);
+    store.addTodo("in pinia");
+    expect(store.list.length).to.equal(1);
+    expect(store.list[0].isDone).to.equal(false);
+    expect(store.list[0].label).to.equal("in pinia");
 
-  const Comp = defineComponent({
-    setup() {
-      return () => createVNode("div");
-    },
+    store.addTodo("dont't need vue");
+    expect(store.list.length).to.equal(2);
   });
 
-  cy.mount(Comp, {
-    global: {
-      plugins: [
-        createTestingPinia({
-          createSpy: piniaSpy,
-        }),
-      ],
-    },
+  it("should delete todo", () => {
+    const store = useTodoList();
+    expect(store.list.length).to.equal(0);
+    store.addTodo("in pinia");
+    store.deleteTodo(store.list[0]);
+    expect(store.list.length).to.equal(0);
   });
-  const store = useTodoList();
+
+  it("should toggle status", () => {
+    const store = useTodoList();
+    store.addTodo("in pinia");
+    expect(store.list[0].isDone).to.equal(false);
+    store.toggleTodo(store.list[0], true);
+    expect(store.list[0].isDone).to.equal(true);
+    store.toggleTodo(store.list[0], false);
+    expect(store.list[0].isDone).to.equal(false);
+  });
+  it("should filter displayList", () => {
+    const store = useTodoList();
+    store.addTodo("todo 1");
+    store.addTodo("done 1");
+    store.toggleTodo(store.list[1], true);
+    store.addTodo("todo 2");
+
+    store.setFilter(TodoFilterEnum.All);
+    expect(store.displayList.length).to.equal(3);
+    store.setFilter(TodoFilterEnum.Active);
+    expect(store.displayList.length).to.equal(2);
+    expect(store.displayList.map((item) => item.label)).not.include("done 1");
+
+    store.setFilter(TodoFilterEnum.Done);
+    expect(store.displayList.length).to.equal(1);
+    expect(store.displayList.map((item) => item.label)).to.include("done 1");
+  });
 });
 
 export {};
