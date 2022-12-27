da56cd4e1b6ca8e4114abc0e82cb59cbcc6d4ea1
jupengfei <1347237169@qq.com>
2022-12-27 19:03:44 +0800

    chore: eslint

--- vue2/.eslintrc.js
+++ vue2/.eslintrc.js
@@ -16,6 +16,8 @@ module.exports = {
   rules: {
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    noImplicitAny: "off",
+    "explicit-module-boundary-types": "off",
   },
   overrides: [
     {
